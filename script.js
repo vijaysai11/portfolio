1085d35c97f98ae56ec508d3e5c7925c5ee7f35b
Vamsi Nakka <vamsi.nakka@cognine.com>
2025-01-29 11:53:01 +0530

    code separated by their respective files

---
+++ script.js
@@ -0,0 +1,27 @@
+document
+  .getElementById("fileInput")
+  .addEventListener("change", function (event) {
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      document.getElementById("profilePic").src = e.target.result;
+    };
+    reader.readAsDataURL(event.target.files[0]);
+  });
+
+// Adding section visibility animation when the section comes into view
+const sections = document.querySelectorAll("section");
+const observerOptions = {
+  threshold: 0.25,
+};
+
+const observer = new IntersectionObserver((entries, observer) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // entry.target.classList.add('visible');
+    }
+  });
+}, observerOptions);
+
+sections.forEach((section) => {
+  observer.observe(section);
+});
